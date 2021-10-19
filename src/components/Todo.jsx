22efffa7a6b4734b23fd28361580b72ef94d04d0
Dianne <mdianne.gabriel@gmail.com>
2021-10-19 09:56:58 -0400

    Update handleDelete to delete the todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,15 +2,20 @@ import React from "react";
 
 const Todo = ({ text, todos, setTodos }) => {
   const handleDelete = (e) => {
-
+    e.preventDefault();
+    setTodos(todos.filter(elem => elem.id !== todos.id))
   };
   return (
     <div className="todo">
       <li className="todo-item">
         {text}
       </li>
-      <button className="complete-btn"><i className="fas fa-check"></i></button>
-      <button className="trash-btn"><i className="fas fa-trash"></i></button>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button className="trash-btn" onClick={handleDelete}>
+        <i className="fas fa-trash"></i>
+      </button>
     </div>
   )
 };
