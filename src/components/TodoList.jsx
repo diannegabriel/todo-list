a615a9e8a1407acdac614a977431392142169b0e
Dianne <mdianne.gabriel@gmail.com>
2021-10-19 09:42:55 -0400

    Map through todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,7 +6,9 @@ const TodoList = ({ todos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        
+        {todos.map(todo => (
+          <Todo />
+        ))}
       </ul>
     </div>
   )
