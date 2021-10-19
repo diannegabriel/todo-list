15cfaa067d59f3e5b45ca23bd97ff0ad0f759e32
Dianne <mdianne.gabriel@gmail.com>
2021-10-19 09:38:00 -0400

    Create Todo component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <div className="todo">
+      <li className="todo-item">
+        HELLO
+      </li>
+      <button className="fas fa-check"></button>
+      <button className="fas fa-trash"></button>
+    </div>
+  )
+};
+
+export default Todo;
