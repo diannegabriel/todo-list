b5b39f2f8aaf97f4952fc502dc4797a566204d4d
Dianne <mdianne.gabriel@gmail.com>
2021-10-19 10:28:59 -0400

    Implement useEffect to go with handleFilter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList';
