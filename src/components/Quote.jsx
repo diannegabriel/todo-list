d35828c7cc8107c5353bbbb759b9cb17a3cdd7bd
Dianne <mdianne.gabriel@gmail.com>
2023-06-28 08:59:29 -0400

    Remove handleRefresh

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
 const Quote = ({ quote }) => {
-  const handleRefresh = () => {
-    console.log("refresh for a new quote!")
-  }
   return (
     <div className="quote-container">
       <p>
       {quote}
       </p>
-      <button className="refresh-btn" onClick={handleRefresh}>
-        <i className="fas fa-redo-alt"></i>
-      </button>
     </div>
   )
 };
