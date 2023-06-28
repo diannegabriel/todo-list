a7f6aa72826fbd405ccb7051ca5e0446fdbebc7e
Dianne <mdianne.gabriel@gmail.com>
2023-06-28 08:37:28 -0400

    Update error container style

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,7 +5,7 @@ const Error = ({ error, inputText, setError }) => {
   return (
     <div className="error-container">
       {error&&inputText.length === 0?
-        <label>Please add a thing to do</label>:""
+        <label>Please add a thing to do!</label>:""
       }
     </div>
   )
