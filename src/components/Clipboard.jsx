b91c64f62dcfd5122151d505cff36147e1d5fd79
Dianne <mdianne.gabriel@gmail.com>
2023-07-25 15:15:43 -0400

    Write the layout of the clipboard

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Clipboard = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <i class="clipboard">
+      <i class="clip"></i>
+    </i>
+  );
+};
 
 export default Clipboard;
