90a0b959513038a29b8e2af212f4ab465674061e
Dianne <mdianne.gabriel@gmail.com>
2023-07-25 21:18:11 -0400

    Style clipboard div

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Clipboard = () => {
   return (
-    <i class="clipboard">
-      <i class="clip"></i>
-    </i>
+    <div className="clipboard"></div>
   );
 };
 
