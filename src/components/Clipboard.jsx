dc7071121099402a25fb8c3b7dc861e02f5162bc
Dianne <mdianne.gabriel@gmail.com>
2023-07-26 09:43:22 -0400

    Add the clip part of the clip board

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Clipboard = () => {
   return (
     <div className="clipboard">
+      <div className="clip"></div>
       <p>THIS IS THE CLIPBOARD</p>
     </div>
   );
