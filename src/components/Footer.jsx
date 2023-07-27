003345d2259cad769440013fdcb04761fd7acea2
Dianne <mdianne.gabriel@gmail.com>
2023-07-27 18:12:43 -0400

    Add footer info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
-      <h1>MotivaDid</h1>
+      <h1 className="project-name">MotivaDid</h1>
       <p>A todo list app with a slight encouragement!</p>
+      <p>
+        Created by: 
+        <a href="https://www.dianne.codes" target="_blank"> Dianne</a>
+      </p>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
