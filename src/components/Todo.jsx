0cf5d4845487bcdb1525aa8d47874d7ef5438b63
Dianne <mdianne.gabriel@gmail.com>
2023-07-27 16:39:36 -0400

    Remote svg element in Todo.jsx and move to App.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -19,18 +19,7 @@ const Todo = ({ text, todo, todos, setTodos }) => {
   };
   return (
     <>
-      <svg id="stroke" xmlns="http://www.w3.org/2000/svg" width="0" height="0">
-        <defs>
-          <path
-            id="line"
-            d="M2 2c49.7 2.6 100 3.1 150 1.7-46.5 2-93 4.4-139.2 7.3 45.2-1.5 90.6-1.8 135.8-.6"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            vector-effect="non-scaling-stroke"
-          />
-        </defs>
-      </svg>
+      
       <div className="todo">
         <li onClick={handleComplete}>
           <a className={`todo-item ${todo.completed ? "completed btn" : ""}`}>
