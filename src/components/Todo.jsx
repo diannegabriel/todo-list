a38da1efc39c50d3f833706382b94b52243b6bcf
Dianne <mdianne.gabriel@gmail.com>
2023-07-27 16:44:05 -0400

    Fix classnames and margin spacings

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -18,27 +18,24 @@ const Todo = ({ text, todo, todos, setTodos }) => {
     setTodos(todos.filter((elem) => elem.id !== todo.id));
   };
   return (
-    <>
-      
-      <div className="todo">
-        <li onClick={handleComplete}>
-          <a className={`todo-item ${todo.completed ? "completed btn" : ""}`}>
-            {text}
-            <svg
-              className={`${todo.completed ? "stroke-active" : "stroke"}`}
-              viewBox="0 0 154 13"
-            >
-              <use href="#line"></use>
-            </svg>
-          </a>
-        </li>
-        <div className="todo-buttons">
-          <button className="erase-button" onClick={handleDelete}>
-            <i className="fas fa-eraser"></i>
-          </button>
-        </div>
+    <div className="todo">
+      <li onClick={handleComplete} className="todo-item">
+        <a className={`${todo.completed ? "completed btn" : ""}`}>
+          {text}
+          <svg
+            className={`${todo.completed ? "stroke-active" : "stroke"}`}
+            viewBox="0 0 154 13"
+          >
+            <use href="#line"></use>
+          </svg>
+        </a>
+      </li>
+      <div className="todo-buttons">
+        <button className="erase-button" onClick={handleDelete}>
+          <i className="fas fa-eraser"></i>
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
